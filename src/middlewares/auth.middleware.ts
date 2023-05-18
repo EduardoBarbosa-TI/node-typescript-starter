54c43ce9709b2e8a393dea16b2dd13f9aefafa79
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-17 21:54:33 -0300

    Startando middlewares para a validação e integridade da aplicação

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,16 +1,47 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import usuarioModel from "../models/usuario.model";
+import { UsuarioInterface } from "../interface/usuario.interface";
 
 class AuthMiddleware {
-    public autorizarUsuarioByToken(req: Request, res: Response, next: NextFunction) {
-        const token = req.query.token || req.headers['x-acess-token'];
+    public async autorizarUsuarioByToken(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        const token = req.query.token || req.headers['x-access-token'];
 
         if(!token) {
-            return res.status(401).send({message: 'Acesso Restrito!'});
+            return res.status(401).send({ message: 'Acesso Restrito!' });
         }
         
-        return next();
+        try{
+            const usuarioToken = jwt.verify(token as string, 'SECRET') as UsuarioInterface;
+            const usuario = await usuarioModel.findById(usuarioToken._id);
+
+            if (!usuario){
+                return res.status(400).send({ message : 'Usuário inexistente!'});
+            }
+
+            req.usuario = usuario;
+
+            return next(); 
+        } catch (error) {
+                return res.status(401).send({ message: 'Token Inválido' });
+        }
     }
 
+    public async autorizarUsuarioByParams(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+            const usuario = await usuarioModel.findById(req.params.id);
+            
+            if (!usuario){
+                return res.status(400).send({ message : 'Usuário inexistente!'});
+            }
+
+            req.usuarioChat = usuario;
+
+            return next(); 
+        } catch (error) {
+                return res.status(401).send({ message: 'Usuário Inválido' });
+        }
+    }
 }
 
 export default new AuthMiddleware();
